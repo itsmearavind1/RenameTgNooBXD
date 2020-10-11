21a2c84f8cab2a62069e86be996ae1d4cd358423
venky619 <67676267+venky619@users.noreply.github.com>
2020-10-11 20:43:04 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -47,7 +47,7 @@ async def progress_for_pyrogram(
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>✅Completed:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>🚀Speed:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n".format(
+        tmp = progress + "✅Completed:{0} \n📁Total Size: {1}\n🚀Speed: {2}/s\n⌚️ETA: {3}\n\n © @super_botz || @leech_group\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
